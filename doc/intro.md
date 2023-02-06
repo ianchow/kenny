d0484f12786ac9d0438acf1913ced42e84c876e5
Ian Chow <ian@ian.my>
2023-02-06 15:56:25 +0800

    yo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kenny/kenny
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kenny/kenny_test.clj
@@ -0,0 +1,7 @@
+(ns kenny.kenny-test
+  (:require [clojure.test :refer :all]
+            [kenny.kenny :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
